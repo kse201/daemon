1e74e0a4abf1946123ed459371842302dc57c8e2
kse201 <kse.201@gmail.com>
2019-02-25 21:49:44 +0900

    add args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
+enum ProcType {
+    DMN,
+    MD,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let subcmd: String = env::args().skip(1).next().unwrap();
+
+    // let proc_type = if subcmd != "md" {
+    // ProcType::MD
+    // } else if subcmd != "dmn" {
+    // ProcType::DMN
+    // } else {
+    // syslog_out();
+    // }
+
+    let proc_type = match &*subcmd {
+        "md" => ProcType::MD,
+        "dmn" => ProcType::DMN,
+        _ => ProcType::DMN,
+    };
 }
